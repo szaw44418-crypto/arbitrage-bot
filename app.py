23c77ad3d538aec8197044716c4969993acca833
szaw44418-crypto <szaw44418@gmail.com>
2026-09-03 15:27:41 +0630

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ TRADE_AMOUNT_USDT = 60.0
 LEVERAGE = 1  
 
 # 🛡️ UPGRADED SAFETY & STRICT PROFITABILITY THRESHOLDS
-MIN_NET_PROFIT_THRESHOLD = 0.45   # 🎯 အနည်းဆုံး 0.45% အသားတင်အမြတ်ကျန်မှသာ Trade လုပ်မည် (Basis Risk & Slippage ကာမိရန်)
+MIN_NET_PROFIT_THRESHOLD = 0.15   # 🎯 အနည်းဆုံး 0.15% အသားတင်အမြတ်ကျန်မှသာ Trade လုပ်မည် (Basis Risk & Slippage ကာမိရန်)
 MIN_24H_VOLUME_USDT = 50,000      # 💧 Liquidity ကောင်းမွန်သော Coin များကိုသာ သုံးမည် ($50,000+)
 
 def truncate_qty(qty, precision):
